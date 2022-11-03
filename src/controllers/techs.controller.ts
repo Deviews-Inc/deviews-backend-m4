5c7ab3d150d8fc2a9587f280d9b5d5adb12a9ef1
Vitor Carvalho <vitors1997@gmail.com>
2022-11-03 12:35:19 -0300

    :rocket: Delete Tech

--- src/controllers/techs.controller.ts
+++ src/controllers/techs.controller.ts
@@ -1,1 +1,11 @@
 //codar aqui
+
+import { Request, Response } from "express";
+import deleteTechService from "../services/techs/deleteTech.service";
+
+export const deleteTechController = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const deletedTech = await deleteTechService(id);
+
+  return res.status(204).json(deletedTech);
+};
