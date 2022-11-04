5097de747f74c7d5e5e8e6a95369e53b783ebab8
anavgbc <agomesbc@gmail.com>
2022-11-04 17:36:42 -0300

    :bug: fixing patch comments

--- src/routes/comments.routes.ts
+++ src/routes/comments.routes.ts
@@ -1,23 +1,11 @@
 import { Router } from "express";
-<<<<<<< Updated upstream
 import { createCommentsController } from "../controllers/comments.controller";
-=======
-import {
-  createCommentsController,
-  deleteCommentController,
-  updateCommentsController,
-} from "../controllers/comments.controllers";
 import isLoggedInMiddleware from "../middlewares/isLoggedIn.middleware";
->>>>>>> Stashed changes
 
-const CommentRoutes = Router();
+const commentRoutes = Router();
 
-<<<<<<< Updated upstream
-CommentRoutes.post("", createCommentsController);
-=======
 commentRoutes.post("/:id", isLoggedInMiddleware, createCommentsController);
 commentRoutes.delete("/:id", isLoggedInMiddleware, deleteCommentController);
 commentRoutes.patch("/:id", isLoggedInMiddleware, updateCommentsController);
->>>>>>> Stashed changes
 
-export default CommentRoutes;
+export default commentRoutes;
